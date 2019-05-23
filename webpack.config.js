22cb0a752e243c3a2036e589af1ec70bf34f36ea
yamachu <yamachu.dev@gmail.com>
2019-05-23 10:51:05 +0900

    add content-script and config to deploy to dist dir

--- webpack.config.js
+++ webpack.config.js
@@ -25,5 +25,16 @@ module.exports = function(env, argv) {
                 ],
             },
         },
+        {
+            entry: './content/content.js',
+            output: {
+                path: path.resolve(__dirname, output_path),
+                filename: 'content.js',
+            },
+            resolve: {
+                extensions: ['.js'],
+            },
+            devtool: argv.mode === 'production' ? false : 'source-map',
+        },
     ];
 };
